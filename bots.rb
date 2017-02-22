beb5e82071df54809ec9f52161463c91c7d38e0f
Nothingtobedevelopedhere <thirteenthfail@gmail.com>
2017-02-22 16:58:50 -0600

    Add files via upload

--- bots.rb
+++ bots.rb
@@ -1,65 +1,134 @@
-require 'twitter_ebooks'
+# Information about a particular Twitter user we know
+class UserInfo
+  attr_reader :username
 
-# This is an example bot definition with event handlers commented out
-# You can define and instantiate as many bots as you like
+  # @return [Integer] how many times we can pester this user unprompted
+  attr_accessor :pesters_left
 
-class MyBot < Ebooks::Bot
-  # Configuration here applies to all MyBots
+  # @param username [String]
+  def initialize(username)
+    @username = username
+    @pesters_left = 1
+  end
+end
+
+class CloneBot < Ebooks::Bot
+  attr_accessor :original, :model, :model_path
+
   def configure
-    # Consumer details come from registering an app at https://dev.twitter.com/
-    # Once you have consumer details, use "ebooks auth" for new access tokens
+    # Configuration for all CloneBots
     self.consumer_key = '3zEYURy0LGgd7x5agEr75jmEN' # Your app consumer key
     self.consumer_secret = 'hMYi9NnNyhDxmb1RTCP1pzk7rwP0ozs2y7AOsi8LoJuXis54bG' # Your app consumer secret
-
-    # Users to block instead of interacting with
-    self.blacklist = ['tnietzschequote']
-
-    # Range in seconds to randomize delay when bot.delay is called
+    self.blacklist = ['kylelehk', 'friedrichsays', 'Sudieofna', 'tnietzschequote', 'NerdsOnPeriod', 'FSR', 'BafflingQuotes', 'Obey_Nxme']
     self.delay_range = 1..6
+    @userinfo = {}
   end
 
+  def top100; @top100 ||= model.keywords.take(100); end
+  def top20;  @top20  ||= model.keywords.take(20); end
+
   def on_startup
-    scheduler.every '1h' do
-      # Tweet something every 24 hours
-      # See https://github.com/jmettraux/rufus-scheduler
-      # tweet("hi")
-      # pictweet("hi", "cuteselfie.jpg")
+    load_model!
+
+    scheduler.cron '0 0 * * *' do
+      # Each day at midnight, post a single tweet
+      tweet(model.make_statement)
     end
   end
 
   def on_message(dm)
-    # Reply to a DM
-    # reply(dm, "secret secrets")
-  end
-
-  def on_follow(user)
-    # Follow a user back
-    # follow(user.screen_name)
+    delay do
+      reply(dm, model.make_response(dm.text))
+    end
   end
 
   def on_mention(tweet)
-    # Reply to a mention
-    # reply(tweet, "oh hullo")
+    # Become more inclined to pester a user when they talk to us
+    userinfo(tweet.user.screen_name).pesters_left += 1
+
+    delay do
+      reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+    end
   end
 
   def on_timeline(tweet)
-    # Reply to a tweet in the bot's timeline
-    # reply(tweet, "nice tweet")
+    return if tweet.retweeted_status?
+    return unless can_pester?(tweet.user.screen_name)
+
+    tokens = Ebooks::NLP.tokenize(tweet.text)
+
+    interesting = tokens.find { |t| top100.include?(t.downcase) }
+    very_interesting = tokens.find_all { |t| top20.include?(t.downcase) }.length > 2
+
+    delay do
+      if very_interesting
+        favorite(tweet) if rand < 0.5
+        retweet(tweet) if rand < 0.1
+        if rand < 0.01
+          userinfo(tweet.user.screen_name).pesters_left -= 1
+          reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+        end
+      elsif interesting
+        favorite(tweet) if rand < 0.05
+        if rand < 0.001
+          userinfo(tweet.user.screen_name).pesters_left -= 1
+          reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+        end
+      end
+    end
   end
 
-  def on_favorite(user, tweet)
-    # Follow user who just favorited bot's tweet
-    # follow(user.screen_name)
+  # Find information we've collected about a user
+  # @param username [String]
+  # @return [Ebooks::UserInfo]
+  def userinfo(username)
+    @userinfo[username] ||= UserInfo.new(username)
   end
 
-  def on_retweet(tweet)
-    # Follow user who just retweeted bot's tweet
-    # follow(tweet.user.screen_name)
+  # Check if we're allowed to send unprompted tweets to a user
+  # @param username [String]
+  # @return [Boolean]
+  def can_pester?(username)
+    userinfo(username).pesters_left > 0
   end
-end
 
-# Make a MyBot and attach it to an account
-MyBot.new("kirchbot") do |bot|
+  # Only follow our original user or people who are following our original user
+  # @param user [Twitter::User]
+  def can_follow?(username)
+    @original.nil? || username.casecmp(@original) == 0 || twitter.friendship?(username, @original)
+  end
+
+  def favorite(tweet)
+    if can_follow?(tweet.user.screen_name)
+      super(tweet)
+    else
+      log "Unfollowing @#{tweet.user.screen_name}"
+      twitter.unfollow(tweet.user.screen_name)
+    end
+  end
+
+  def on_follow(user)
+    if can_follow?(user.screen_name)
+      follow(user.screen_name)
+    else
+      log "Not following @#{user.screen_name}"
+    end
+  end
+
+  private
+  def load_model!
+    return if @model
+
+    @model_path ||= "model/kircheis.model"
+
+    log "Loading model #{model_path}"
+    @model = Ebooks::Model.load(model_path)
+  end
+  
+  CloneBot.new("kirchbot") do |bot|
   bot.access_token = "834226336495521792-HpXGQfnq7n77Kl0N34G1yb6cEN3AY29" # Token connecting the app to this account
   bot.access_token_secret = "1J4P7RijjwWjyytxcPqLTDfQVziyZkWYUXtwsn9dKcbuu" # Secret connecting the app to this account
+
+  bot.original = "thisismyhomenow"
+end
 end
